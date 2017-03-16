8287577c8f2dfe8d54c054eb474ac48ab18e4234
melaanya <ankaberg@yandex.ru>
2017-03-16 11:48:45 +0300

    CFG code addded

--- src/DataFlowAnalysis/IntermediateRepresentation/ControlFlowGraph/Graph.cs
+++ src/DataFlowAnalysis/IntermediateRepresentation/ControlFlowGraph/Graph.cs
@@ -4,9 +4,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using QuickGraph;
+using QuickGraph.Algorithms.Search;
+
+using DataFlowAnalysis.BasicBlockCode.Model;
+
 namespace DataFlowAnalysis.ControlFlowGraph
 {
-    public class Graph
-    {
-    }
+	public class Graph
+	{
+		public BidirectionalGraph<BasicBlock, Edge<BasicBlock>> CFG =
+		  new BidirectionalGraph<BasicBlock, Edge<BasicBlock>>();
+
+		public Graph() { }
+
+		// returns null if such id doesn't exist
+		public BasicBlock getBlockById(int id)
+		{
+			foreach (var v in CFG.Vertices)
+			{
+				if (v.BlockId == id) return v;
+			}
+			return null;
+		}
+
+		// constructor from recently prepared BasicBlocksList
+		public Graph(BasicBlocksList listBlocks)
+		{
+			CFG.AddVertexRange(listBlocks.Blocks);
+			//CFG.Vertices.ToList().ForEach(Console.WriteLine);
+			foreach (var block in listBlocks.Blocks)
+			{
+				foreach (var numIn in block.InputBlocks)
+				{
+					CFG.AddEdge(new Edge<BasicBlock>(this.getBlockById(numIn), block));
+				}
+			}
+		}
+
+		// get BasicBlocksList of all ancestors of the block
+		public BasicBlocksList getAncestors(int id)
+		{
+			var result = new BasicBlocksList();
+			foreach (var v in CFG.Vertices)
+			{
+				if (v.BlockId == id)
+				{
+					foreach (var edge in CFG.InEdges(v))
+					{
+						result.Add(edge.Target);
+					}
+				}
+			}
+			return result;
+		}
+
+		// get BasicBlocksList of all predecessors of the block
+		public BasicBlocksList getParents(int id)
+		{
+			BasicBlocksList blockList = new BasicBlocksList();
+			foreach (var v in CFG.Vertices)
+			{
+				if (v.BlockId == id)
+				{
+					foreach (var edge in CFG.InEdges(v))
+					{
+						blockList.Add(edge.Source);
+					}
+				}
+			}
+			return blockList;
+		}
+
+		public override string ToString()
+		{
+			string res = "";
+			foreach (var v in CFG.Vertices)
+			{
+				res += v.BlockId;
+				foreach (var e in CFG.OutEdges(v))
+				{
+					res += " " + e.Target.BlockId;
+				}
+				foreach (var e in CFG.InEdges(v))
+				{
+					res += " " + e.Source.BlockId;
+				}
+				res += "\n";
+			}
+			return res;
+		}
+	}
 }
