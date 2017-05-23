d0fbcf576c33c644901808e39c0084821bd9c6d7
Voloshin Bogdan <ya.bovi2012@yandex.ru>
2017-05-24 01:16:48 +0300

    Add MOP algorithm #16

---
+++ src/DataFlowAnalysis/MeetOverPaths/MeetOverPaths.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataFlowAnalysis.IterativeAlgorithm.IterativeAlgorithmParameters;
+using DataFlowAnalysis.IntermediateRepresentation.BasicBlockCode.Model;
+using DataFlowAnalysis.IntermediateRepresentation.ControlFlowGraph;
+using DataFlowAnalysis.IterativeAlgorithm;
+
+namespace DataFlowAnalysis.MeetOverPaths
+{
+    public class MeetOverPaths
+    {
+        public static Dictionary<int, V> Apply<V>(Graph graph, BasicIterativeAlgorithmParameters<V> param, Dictionary<int, int> order = null)
+        {
+            var MOP = new Dictionary<int, V>();
+
+            foreach (BasicBlock blockTo in graph)
+            {
+                MOP[blockTo.BlockId] = param.FirstValue;
+                foreach (var path in GraphAlgorithms.FindAllPaths(graph, blockTo.BlockId))
+                {
+                    var value = path.Aggregate(param.StartingValue, param.TransferFunction);
+                    MOP[blockTo.BlockId] = param.GatherOperation(new List<V> { MOP[blockTo.BlockId], value});
+                }
+
+            }
+
+            return MOP;
+        }
+
+    }
+}
