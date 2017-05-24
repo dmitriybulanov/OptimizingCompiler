6d52ace9f8103bafbdf428f62dfeff24e21f4f54
Alexander Zakharenko <wisestump@yandex.ru>
2017-05-24 03:58:40 +0300

    Add TransferFunctionStorage test

---
+++ src/UnitTests/TransferFunctionStorageTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using DataFlowAnalysis.IntermediateRepresentation.BasicBlockCode;
+using DataFlowAnalysis.IntermediateRepresentation.ControlFlowGraph;
+using DataFlowAnalysis.IntermediateRepresentation.Regions;
+using DataFlowAnalysis.IntermediateRepresentation.ThreeAddressCode;
+using DataFlowAnalysis.IterativeAlgorithm;
+using DataFlowAnalysis.Utilities;
+using GPPGParser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SyntaxTree.SyntaxNodes;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TransferFunctionStorageTest
+    {
+        [TestMethod]
+        public void TransferFunctionStorage()
+        {
+string text = @"
+    i = 1;
+    j = 4;
+    a = 2;
+    while i < 20
+    {  
+        i = i + 1;
+        j = j + 1;
+        if i > a
+            a = a + 5;
+        i = i + 1;
+    }";
+            SyntaxNode root = ParserWrap.Parse(text);
+            var graph = new Graph(BasicBlocksGenerator.CreateBasicBlocks(ThreeAddressCodeGenerator.CreateAndVisit(root).Program));
+            var regions = new RegionSequence().CreateSequence(graph);
+            var storage = new TransferFunctionStorage<int>();
+            for (var i = 0; i < regions.Count - 1; i++)
+            {
+                storage[regions[i], RegionDirection.Out, regions[i + 1]] = x => i;
+                Assert.IsTrue(storage[regions[i], RegionDirection.Out, regions[i + 1]](0) == i);
+
+                storage[regions[i], RegionDirection.In, regions[i + 1]] = x => i;
+                Assert.IsTrue(storage[regions[i], RegionDirection.Out, regions[i + 1]](0) == i);
+            }
+        }
+    }
+}
