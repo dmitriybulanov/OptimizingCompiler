1869b09b507f4b0954002b40e03fb73812650a09
Bogdan Voloshin <ya.bovi2012@yandex.ru>
2017-05-24 15:49:09 +0300

    Add tests for retreating and reverse edges

---
+++ src/UnitTests/IntermediateRepresentationTests/RetreatingNotReverseEdgesTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataFlowAnalysis.IntermediateRepresentation.CheckRetreatingIsReverse;
+using SyntaxTree.SyntaxNodes;
+using DataFlowAnalysis.IntermediateRepresentation.ControlFlowGraph;
+using GPPGParser;
+using DataFlowAnalysis.IntermediateRepresentation.BasicBlockCode.Model;
+using DataFlowAnalysis.IntermediateRepresentation.BasicBlockCode;
+using DataFlowAnalysis.IntermediateRepresentation.ThreeAddressCode;
+using System.Diagnostics;
+
+namespace UnitTests.IntermediateRepresentationTests
+{
+    [TestClass]
+    public class RetreatingNotReverseEdgesTests
+    {
+        [TestMethod]
+        public void RetreatingNotReverseEdges1()
+        {
+            var programText = File.ReadAllText("./../../RetreatingNotReverseEdgesEx1.txt");
+
+
+            SyntaxNode root = ParserWrap.Parse(programText);
+            Graph graph = new Graph(
+                BasicBlocksGenerator.CreateBasicBlocks(
+                    ThreeAddressCodeGenerator.CreateAndVisit(root).Program));
+
+            Assert.IsFalse(CheckRetreatingIsReverse.CheckReverseEdges(graph));
+        }
+
+        [TestMethod]
+        public void RetreatingNotReverseEdges2()
+        {
+            var programText = File.ReadAllText("./../../RetreatingNotReverseEdgesEx2.txt");
+
+            SyntaxNode root = ParserWrap.Parse(programText);
+            Graph graph = new Graph(
+                BasicBlocksGenerator.CreateBasicBlocks(
+                    ThreeAddressCodeGenerator.CreateAndVisit(root).Program));
+
+            Assert.IsFalse(CheckRetreatingIsReverse.CheckReverseEdges(graph));
+        }
+
+        [TestMethod]
+        public void RetreatingNotReverseEdges3()
+        {
+            var programText = File.ReadAllText("./../../RetreatingNotReverseEdgesEx3.txt");
+
+            SyntaxNode root = ParserWrap.Parse(programText);
+            Graph graph = new Graph(
+                BasicBlocksGenerator.CreateBasicBlocks(
+                    ThreeAddressCodeGenerator.CreateAndVisit(root).Program));
+
+            Assert.IsFalse(CheckRetreatingIsReverse.CheckReverseEdges(graph));
+        }
+    }
+}
